dd95e82d9d30a1605902e4a51c564a5f3cded0d4
bunyodjon002 <mirxalilovbunyod009@gmail.com>
2023-08-14 10:28:36 +0500

    coder

---
+++ src/main/java/uz/maktab/IT/maktabIT/service/dto/AStudentDTO.java
@@ -0,0 +1,82 @@
+package uz.maktab.IT.maktabIT.service.dto;
+
+import java.time.LocalDate;
+
+public class StudentDTO {
+    private Long id;
+    private String name;
+    private String Surname;
+    private String Name_Patronymic_name;
+    private LocalDate young;
+    private String Login;
+    private String password;
+
+    public StudentDTO() {
+    }
+
+    public StudentDTO(Long id, String name, String surname, String name_Patronymic_name, LocalDate young, String login, String password) {
+        this.id = id;
+        this.name = name;
+        Surname = surname;
+        Name_Patronymic_name = name_Patronymic_name;
+        this.young = young;
+        Login = login;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return Surname;
+    }
+
+    public void setSurname(String surname) {
+        Surname = surname;
+    }
+
+    public String getName_Patronymic_name() {
+        return Name_Patronymic_name;
+    }
+
+    public void setName_Patronymic_name(String name_Patronymic_name) {
+        Name_Patronymic_name = name_Patronymic_name;
+    }
+
+    public LocalDate getYoung() {
+        return young;
+    }
+
+    public void setYoung(LocalDate young) {
+        this.young = young;
+    }
+
+    public String getLogin() {
+        return Login;
+    }
+
+    public void setLogin(String login) {
+        Login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
